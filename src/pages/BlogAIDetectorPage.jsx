ba17912dc51ed38d457cf517f60ac9d4bee3bd83
ankamdharmendar1-ui <dharmendarankam@gmail.com>
2026-06-28 10:03:06 +0530

    Fix build import error

--- src/pages/BlogAIDetectorPage.jsx
+++ src/pages/BlogAIDetectorPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SEO } from '../lib/seoHelper';
+import SEO from '../lib/seoHelper';
 import { Link } from 'react-router-dom';
 
 const BlogAIDetectorPage = () => (
